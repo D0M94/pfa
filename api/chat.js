f22123ef7013798bae9d5f5c2a37004dbae04cf2
Dom <dominik.zvara88@gmail.com>
2026-04-13 21:29:40 +0200

    fixed prev upload

--- api/chat.js
+++ api/chat.js
@@ -10,5 +10,6 @@ export default async function handler(req, res) {
     body: JSON.stringify(req.body)
   });
   const data = await response.json();
+  console.log(JSON.stringify(data)); // ← add this
   res.status(200).json(data);
 }
